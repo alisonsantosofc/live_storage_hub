e15f3a5a8e23eabb22744e25d163ee4cc771a11e
alisonsantosofc <alisonsantoso22@gmail.com>
2026-02-01 12:15:12 -0300

    build: finished docker infra

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,22 @@
-FROM eclipse-temurin:17-jdk-alpine AS build
-WORKDIR /app
-COPY . .
-RUN ./mvnw clean package -DskipTests
+# ---------- BUILD ----------
+FROM maven:3.9.9-eclipse-temurin-17 AS build
 
-FROM eclipse-temurin:17-jre-alpine
 WORKDIR /app
+
+COPY pom.xml .
+RUN mvn dependency:go-offline
+
+COPY src ./src
+RUN mvn clean package -DskipTests
+
+
+# ---------- RUNTIME ----------
+FROM eclipse-temurin:17-jre-alpine
+
+WORKDIR /app
+
 COPY --from=build /app/target/*.jar app.jar
 
 EXPOSE 9090
+
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/alisondev/live_storage_hub/LiveStorageHubApplication.java
+++ src/main/java/com/alisondev/live_storage_hub/LiveStorageHubApplication.java
@@ -5,23 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class LiveStorageHubApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-		// Load aws keys
-		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
-
-		// Load jwt secret key
-		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
-
-		// Load admin api secret key
-		System.setProperty("ADMIN_SUPER_SECRET_KEY", dotenv.get("ADMIN_SUPER_SECRET_KEY"));
-
 		SpringApplication.run(LiveStorageHubApplication.class, args);
 	}
 
